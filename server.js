927db1b5c1e36b3ecc1d7309fc8d9f6a7212b66f
Emanuelly <sandramarinhos78@gmail.com>
2019-06-25 17:38:04 -0300

    usando o req e res

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express'); // importanto o express que retorna uma função
-
+//chamando a função que foi importada
 const app = express();
+
+//toda vez que o usuario acessar a rota raiz, exibirá  que for feito.
+// esse get passa uma função que recebe dois parâmetros: req(requisição do servidor, contem os detalhes e informaçoes da requisição)  
+//e res(a resposta que damos a requisição.) faz a requisição e o servidor dar uma resposta.
+app.get('/', (req, res) => {
+    res.send('HELLOOOOW')
+});
+
+
+
+//dizendo que a aplicação tem que ouvir a porta 3001 da web
+app.listen(3001);

89ee64463537148d21f28393108495074779bd4f
Emanuelly <sandramarinhos78@gmail.com>
2019-06-26 13:18:49 -0300

    adicionando script no arquivo package.json

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require('express'); // importanto o express que retorna uma função
-//chamando a função que foi importada
+// import das funções que usamos
+const express = require('express'); 
+const mongoose = require('mongoose');
+
+//Iniciando o app
 const app = express();
+
+//Iniciando o DB
+//a url usada junto com a porta e o banco usado (pode mudar caso haja senha no banco), passa um objeto de parametr
+const URI = 'mongodb://localhost:27017/nodeapi';
+const OPTS = { useNewUrlParser: true };
+
+mongoose.connect(URI, OPTS, function(err) {
+  if (err) { return console.error('failed' + err);}
+}); 
+
 
 /*CRIANDO UMA ROTA: Toda vez que o usuario acessar a rota raiz, exibirá  que for feito.
 esse get passa uma função que recebe dois parâmetros: req(requisição do servidor, contem os detalhes e informaçoes da requisição)  
@@ -8,7 +21,7 @@ e res(a resposta que damos a requisição.) faz a requisição e o servidor dar 
 */
 app.get('/', (req, res) => {
     //passando uma resposta para a requisição
-    res.send('HELLOOOOW')
+    res.send('cansadissima')
 });
 
 
